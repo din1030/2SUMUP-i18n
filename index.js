78238ed505eeeee5e3f4864fd3de86489145284f
Din <din1030@gmail.com>
2020-08-12 04:19:56 +0800

    add download link

--- index.js
+++ index.js
@@ -6,5 +6,24 @@ for (var k in data) {
     en[k] = data[k][0];
     tw[k] = data[k][1];
 }
-enJson.append(JSON.stringify(en, null, 4));
-twJson.append(JSON.stringify(tw, null, 4));
+
+enStr = JSON.stringify(en, null, 4);
+twStr = JSON.stringify(tw, null, 4);
+
+enJson.append(enStr);
+twJson.append(twStr);
+
+// let en = JSON.stringify(en);
+let dataUri1 = 'data:application/json;charset=utf-8,' + encodeURIComponent(enStr);
+let exportFileDefaultName1 = 'EN.json';
+
+let enDownload = document.querySelector('#enDownload');
+enDownload.setAttribute('href', dataUri1);
+enDownload.setAttribute('download', exportFileDefaultName1);
+
+let dataUri2 = 'data:application/json;charset=utf-8,' + encodeURIComponent(twStr);
+let exportFileDefaultName2 = 'TC.json';
+
+let twDownload = document.querySelector('#twDownload');
+twDownload.setAttribute('href', dataUri2);
+twDownload.setAttribute('download', exportFileDefaultName2);
